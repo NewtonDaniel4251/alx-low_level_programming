36a0a3e85ae34ac43dcdb30d00ddc14b5e45802d
Daniel Omotayo Ajinde <omotayo.ajinde@gmail.com>
2023-02-23 14:17:06 -0800

    okay

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints Fizz, Buzz and FizzBuzz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			printf("Fizz");
+			putchar(' ');
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+			putchar(' ');
+		}
+		else if ((i % 3) == 0 && (i % 5) == 0)
+		{
+			printf("FizzBuzz");
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d", i);
+			putchar(' ');
+		}
+	}
+}
